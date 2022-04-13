fba509a701b44bc4140931af5669d83be6f0d4ac
John Lervandal <55460368+JohnLervandal@users.noreply.github.com>
2022-04-13 18:52:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -305,7 +305,8 @@ app.post('/courses/byKeyword',
     // show list of courses in a given subject
     async(req, res, next) => {
         const { keyword } = req.body;
-        const courses = await Course.find({name:keyword}, {independent_study: false }).sort({ term: 1, num: 1, section: 1 })
+        const courses = await Course.find({name: keyword}, {independent_study: false }).sort({ term: 1, num: 1, section: 1 })
+        
 
         res.locals.courses = courses
         res.locals.strTimes = courses.strTimes
